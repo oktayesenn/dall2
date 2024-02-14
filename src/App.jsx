fc48daa19d3eebda98301a274a1c78af9677a50c
oktayesenn <oktayesenn@gmail.com>
2024-02-13 19:33:25 -0500

    Changed applied

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,7 @@ const App = () => (
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
 
-<<<<<<< HEAD
       <Link to="/create-post" className="font-inter font-medium bg-[#000000] text-white px-4 py-2 rounded-md">Create</Link>
-=======
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
->>>>>>> 1b355782482ab970f53ec7971ac6282a1b00d9d2
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
